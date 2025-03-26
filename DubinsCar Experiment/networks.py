b41d6b84b10f16d144db31fb7d301d067e4e2000
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-03-26 11:07:27 +0100

    change net structure

--- DubinsCar Experiment/networks.py
+++ DubinsCar Experiment/networks.py
@@ -2,61 +2,45 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class NeuralDubinsCar(nn.Module):
+
     def __init__(self):
         super(NeuralDubinsCar, self).__init__()
         self.fc1 = nn.Linear(3, 128, bias=True)
         self.fc2 = nn.Linear(128, 128, bias=True)
         self.fc3 = nn.Linear(128, 3, bias=True)
-        #self.fc4 = nn.Linear(32, 16, bias=True)
-        #self.fc5 = nn.Linear(16, 3, bias=True)
-
-        self.fc_res = nn.Linear(3, 3, bias=True)
 
     def forward(self, x):
 
-        residual = self.fc_res(x)
+        residual = x
 
         x = self.fc1(x)
         x = F.sigmoid(x)
         x = self.fc2(x)
         x = F.sigmoid(x)
         x = self.fc3(x)
-        #x = F.sigmoid(x)
-        #x = self.fc4(x)
-        #x = F.sigmoid(x)
-        #x = self.fc5(x)
 
         x = x + residual
-
         return x
 
 class NeuralPendulum(nn.Module):
+
     def __init__(self):
         super(NeuralPendulum, self).__init__()
         self.fc1 = nn.Linear(2, 64, bias=True)
         self.fc2 = nn.Linear(64, 64, bias=True)
-        self.fc3 = nn.Linear(64, 32, bias=True)
-        self.fc4 = nn.Linear(32, 16, bias=True)
-        self.fc5 = nn.Linear(16, 2, bias=True)
-
-        #self.fc_res = nn.Linear(2, 2, bias=True)
+        self.fc3 = nn.Linear(64, 2, bias=True)
 
     def forward(self, x):
 
-        #residual = self.fc_res(x)
+        residual = x
 
         x = self.fc1(x)
         x = F.sigmoid(x)
         x = self.fc2(x)
         x = F.sigmoid(x)
         x = self.fc3(x)
-        x = F.sigmoid(x)
-        x = self.fc4(x)
-        x = F.sigmoid(x)
-        x = self.fc5(x)
 
-        #x = x + residual
-
+        x = x + residual
         return x
 
 
